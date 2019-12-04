be55f7f3b2bb8ce064cc663225b42040d7c6bcb2
ghilbing <ghilbing@gmail.com>
2019-12-04 08:35:51 -0800

    Initial commit - Navigation Drawer working

---
+++ app/src/main/java/com/hilbing/mybands/MainActivity.java
@@ -0,0 +1,142 @@
+package com.hilbing.mybands;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigationView_NV)
+    NavigationView navigationViewNV;
+    @BindView(R.id.drawerLayout_DL)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.events_RV)
+    RecyclerView recyclerView;
+    Toolbar mToolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+
+    @SuppressLint("WrongConstant")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+       /* ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();*/
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      //  View navView = navigationViewNV.inflateHeaderView(R.layout.nav_header);
+
+        navigationViewNV.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                userSelector(menuItem);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+
+    private void userSelector(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+             //   getSupportActionBar().setTitle("Home");
+                //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+            //    getSupportActionBar().setTitle("Profile");
+                //   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.nav_instruments:
+                Toast.makeText(this, "Instruments", Toast.LENGTH_SHORT).show();
+                //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InstrumentFragment()).commit();
+                break;
+            case R.id.nav_band:
+                Toast.makeText(this, "Bands", Toast.LENGTH_SHORT).show();
+                //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BandFragment()).commit();
+                break;
+            case R.id.nav_song:
+                Toast.makeText(this, "Songs", Toast.LENGTH_SHORT).show();
+                //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SongFragment()).commit();
+                break;
+            case R.id.nav_playlists:
+                Toast.makeText(this, "Playlists", Toast.LENGTH_SHORT).show();
+                //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PlaylistFragment()).commit();
+                break;
+            case R.id.nav_rehearsals:
+                Toast.makeText(this, "Rehearsals", Toast.LENGTH_SHORT).show();
+                //     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RehearsalFragment()).commit();
+                break;
+            case R.id.nav_concerts:
+                Toast.makeText(this, "Concerts", Toast.LENGTH_SHORT).show();
+                //     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConcertFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                //    Toast.makeText(getApplicationContext(), "Share option menu", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                //    Toast.makeText(getApplicationContext(), "Settings option menu", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                //    Toast.makeText(getApplicationContext(), "Settings option menu", Toast.LENGTH_LONG).show();
+                break;
+            }
+         //   drawerLayout.closeDrawer(GravityCompat.START);
+            //return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
